e4ef2349eea3df83692e9023a96cc006b54de8c8
Nicolas <nicolashorne99@gmail.com>
2025-01-27 13:27:46 -0500

    TableRow woes

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,7 +1,14 @@
 "use client"
 
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+
+
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 export const Editor=() => {
     const editor = useEditor({
         editorProps:{
@@ -10,7 +17,16 @@ export const Editor=() => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
         },
-        extensions: [StarterKit],
+        extensions: [StarterKit,
+            Table,
+            TableCell,
+            TableHeader,
+ 
+            TaskItem.configure({
+                nested:true,
+            }),
+            TaskList
+        ],
         content: '<p>Hello World! 🌎️</p>',
       })
     return (
