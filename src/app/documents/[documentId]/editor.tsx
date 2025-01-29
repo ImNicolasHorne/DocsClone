7a1df53a1458ed375b4510ca18d95562539e065d
Nicolas <nicolashorne99@gmail.com>
2025-01-29 17:11:25 -0500

    globallAttricute.types.forEach?

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -6,12 +6,21 @@ import TableHeader from '@tiptap/extension-table-header'
 import { TableRow } from '@tiptap/extension-table-row'; 
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
+import TextAlign from '@tiptap/extension-text-align'
+import Underline from '@tiptap/extension-underline'
+import Strike from '@tiptap/extension-strike'
+import Link from '@tiptap/extension-link'
+import { Color } from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
 
 import { useEditorStore  } from '@/store/use-editor-store';
+import { FontSizeExtension } from '@/extensions/font-size';
 
 export const Editor=() => {
   const { setEditor } = useEditorStore();
@@ -48,7 +57,25 @@ export const Editor=() => {
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             },
         },
-        extensions: [StarterKit,
+        extensions: [
+            StarterKit,
+            FontSizeExtension,
+            TextAlign.configure({
+              types: ["heading", "paragraph"]
+            }),
+            Link.configure({
+              openOnClick: false,
+              autolink: true,
+              defaultProtocol: "https"
+            }),
+            Color,
+            Highlight.configure({
+              multicolor: true,
+            }),
+            FontFamily,
+            TextStyle,
+            Underline,
+            Strike,
             Image,
             ImageResize,
             Table,
