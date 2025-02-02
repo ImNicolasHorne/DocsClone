1d352041e0463b69262746e0ffd40fd3600fe5de
Nicolas <nicolashorne99@gmail.com>
2025-02-02 17:55:13 -0500

    Clerk broke me

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://heroic-wallaby-54.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}> 
+        return <ConvexProviderWithClerk useAuth={useAuth} client={convex}>{children}</ConvexProviderWithClerk>;
+    </ClerkProvider>
 }
