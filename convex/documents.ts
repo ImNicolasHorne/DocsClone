ac1c8c8ec802ea7be80e427f8e2b775f70aef098
Nicolas <nicolashorne99@gmail.com>
2025-02-03 11:59:33 -0500

    figure out initialContent issue

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,8 +3,10 @@ import { v } from "convex/values"
 import { mutation, query } from "./_generated/server";
 
 export const create = mutation({
-    args: { title: v.optional(v.string(), initialContent: v.optional(v.string()) ) },
-    handler: async () => {},
+    args: { title: v.optional(v.string()), initialContent: v.optional(v.string())  },
+    handler: async (ctx, args) => {
+        
+    },
 })
 
 export const get = query({
